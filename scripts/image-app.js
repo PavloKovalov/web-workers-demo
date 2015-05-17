44d855d438a15a313d892f72a09f4739940f1dd5
Pavel Kovalyov <sxe.funky@gmail.com>
2015-05-17 13:14:45 +0300

    Moved image manipulation to worker

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -1,10 +1,12 @@
-(function(){
+(function() {
   // http://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
   var original;
   var imageLoader = document.querySelector('#imageLoader');
   imageLoader.addEventListener('change', handleImage, false);
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
+
+  var worker = new Worker('scripts/worker.js');
 
   function handleImage(e){
     var reader = new FileReader();
@@ -40,23 +42,26 @@
 
     toggleButtonsAbledness();
 
-    // Hint! This is where you should post messages to the web worker and
-    // receive messages from the web worker.
+    worker.postMessage({imageData: imageData, type: type});
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
-    }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+    worker.onmessage = function(event) {
+      var image = event.data;
+      toggleButtonsAbledness();
+      if (image) {
+        return ctx.putImageData(image, 0, 0);
+      }
+      console.log('Nothing returned from worker');
+    };
+
+    worker.onerror = function(error) {
+      function WorkerException(message) {
+        this.name = 'WorkerException';
+        this.message = message;
+      }
+      throw new WorkerException('Worker error');
+    };
+
+
   };
 
   function revertImage() {
